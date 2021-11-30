40b4d8038cd78a597c20c3ec1073ae17741ea6e7
XiaoxinPro_WSL_Ubuntu <heruilang@foxmail.com>
2021-12-01 01:01:17 +0800

    update note & comment format

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,16 @@
+/**
+ * @copyright: uintchar
+ * @version: v1.0
+ * @author: uintchar
+ * @date: 2021-12-01
+ * @description:
+ *  - first
+ *  - second
+ * @revision:
+ *  - last revised by uintchar on 2021-12-01
+ *  - last revised by *** on ***
+ **/
+
 #include <iostream>
 
 #include <stdio.h>
--- src/test.h
+++ src/test.h
@@ -1,8 +1,32 @@
+/**
+ * @copyright: uintchar
+ * @version: v1.0
+ * @author: uintchar
+ * @date: 2021-12-01
+ * @description:
+ *  - first
+ *  - second
+ * @revision:
+ *  - last revised by uintchar on 2021-12-01
+ *  - last revised by *** on ***
+ **/
+
 #ifndef TEST_H
 #define TEST_H
 
 #include <iostream>
 
+/**
+ * @brief:
+ *  - first
+ *  - second
+ * @param:
+ *  - first
+ *  - second
+ * @return:
+ *  - first:
+ *  - second:
+ **/
 void testFunc1(void);
 
 #endif
