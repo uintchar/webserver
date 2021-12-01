bbee0136469a3953c6ddbf3b7d75a95640f24fd5
XiaoxinPro_WSL_Ubuntu <heruilang@foxmail.com>
2021-12-01 12:25:56 +0800

    update note

--- src/main.cpp
+++ src/main.cpp
@@ -11,19 +11,85 @@
  *  - last revised by *** on ***
  **/
 
-#include <iostream>
+// #include <iostream>
 
+// #include <stdio.h>
+
+// #include "test.h"
+
+// int main()
+// {
+//   std::cout << "main start" << std::endl;
+
+//   testFunc1();
+
+//   std::cout << "main end" << std::endl;
+
+//   return 0;
+// }
+
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
-#include "test.h"
-
-int main()
+int main(int argc, char *argv[])
 {
-  std::cout << "main start" << std::endl;
+  // 创建管道
+  int pipefd[2] = {0};
+  if (pipe(pipefd) == -1)
+  {
+    perror("pipe");
+    exit(EXIT_FAILURE);
+  }
 
-  testFunc1();
+  // 创建子进程
+  pid_t pid = fork();
+  if (pid == -1)
+  {
+    perror("fork");
+    exit(EXIT_FAILURE);
+  }
 
-  std::cout << "main end" << std::endl;
+  // 父进程向管道写消息：从标准输入获取消息并发送给子进程，当输入 quit 时退出
+  if (pid > 0)
+  {
+    close(pipefd[0]);
+    char wbuf[1024] = {0};
+
+    while (1)
+    {
+      fgets(wbuf, 1024, stdin);
+      write(pipefd[1], wbuf, strlen(wbuf) + 1);
+      if (strcmp(wbuf, "quit\n") == 0)
+        break;
+      memset(wbuf, 0, 1024);
+    }
+
+    close(pipefd[1]);
+    exit(EXIT_SUCCESS);
+  }
+  // 子进程从管道读消息并将其打印到标准输出中
+  else
+  {
+    close(pipefd[1]);
+    char rbuf[1024] = {0};
+
+    while (1)
+    {
+      read(pipefd[0], rbuf, 1024);
+      printf("recv from parent:");
+      fputs(rbuf, stdout);
+      if (strcmp(rbuf, "quit\n") == 0)
+        break;
+      memset(rbuf, 0, 1024);
+    }
+
+    close(pipefd[0]);
+    exit(EXIT_SUCCESS);
+  }
 
   return 0;
 }
